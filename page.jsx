a173b22d95d6a8c397586c30b070c7dce1eee9c9
Sanjaysathu <57537809+Sanjaysathu@users.noreply.github.com>
2024-05-01 12:05:13 +0530

    removed unused code

---
+++ page.jsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import { headers } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import { SubmitButton } from "./submit-button";
+import AuthButton from "@/components/AuthButton";
+
+export default function Login({ searchParams }) {
+  const signIn = async (formData) => {
+    "use server";
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return redirect("/login?message=Could not authenticate user");
+    }
+
+    return redirect("/booth-details");
+  };
+
+  // const signUp = async (formData) => {
+  //   "use server";
+
+  //   const origin = headers().get("origin");
+  //   const email = formData.get("email");
+  //   const password = formData.get("password");
+  //   const supabase = createClient();
+
+  //   const { error } = await supabase.auth.signUp({
+  //     email,
+  //     password,
+  //     options: {
+  //       emailRedirectTo: `${origin}/auth/callback`,
+  //     },
+  //   });
+
+  //   if (error) {
+  //     return redirect("/login?message=Could not authenticate user");
+  //   }
+
+  //   return redirect("/login?message=Check email to continue sign in process");
+  // };
+
+  return (
+    <>
+      <nav className="w-full flex border-b border-b-foreground/10 h-16">
+        <div className="self-center font-semibold text-xl px-4">
+          <a href="/">Voter Sahyog</a>
+        </div>
+      </nav>
+
+      <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+        <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+          <label className="text-md" htmlFor="email">
+            Email
+          </label>
+          <input className="rounded-md px-4 py-2 bg-inherit border mb-6" name="email" placeholder="you@example.com" required />
+          <label className="text-md" htmlFor="password">
+            Password
+          </label>
+          <input className="rounded-md px-4 py-2 bg-inherit border mb-6" type="password" name="password" placeholder="••••••••" required />
+          <SubmitButton formAction={signIn} className=" bg-black rounded-md px-4 py-2 text-white mb-2" pendingText="Signing In...">
+            Sign In
+          </SubmitButton>
+          {/* <SubmitButton formAction={signUp} className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2" pendingText="Signing Up...">
+          Sign Up
+        </SubmitButton> */}
+          {searchParams?.message && <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">{searchParams.message}</p>}
+        </form>
+      </div>
+    </>
+  );
+}
