dc15ac99f8b6f0a9586a4c2d175ed8b65811639f
Sanjaysathu <57537809+Sanjaysathu@users.noreply.github.com>
2024-05-01 16:42:15 +0530

    added west bengal official symbol

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,12 +6,17 @@ const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Voter Sahyog",
-  description: "Stay updated with live data on people standing in polling booths",
+  description: "Dashboard to know about live count of people standing in polling station",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={GeistSans.className}>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.png" type="image/png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" type="image/png" sizes="180x180" />
+      </head>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">{children}</main>
       </body>
