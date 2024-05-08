98fd5eba44c7d6403009dae117cb1ceca44504e9
Sanjaysathu <57537809+Sanjaysathu@users.noreply.github.com>
2024-05-08 13:09:11 +0530

    Updated title

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` 
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Voter Sahyog",
+  title: "Voter Sahayata Portal",
   description: "Dashboard to know about live count of people standing in polling station",
 };
 
